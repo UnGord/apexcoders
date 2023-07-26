4106c4d11472e56ac54e65d95934eb3583bd6621
UnGord <odujide35@gmail.com>
2023-07-26 09:50:00 +0100

    debug off

--- apexcoders/settings.py
+++ apexcoders/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f=+@@+-=br7q!h5l=#g%$xw976i6)d*68=+n6g5v!+wo23#%i!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['16.171.132.58']
 
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
